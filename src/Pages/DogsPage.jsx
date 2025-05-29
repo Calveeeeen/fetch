66d4f770ce4063e7757eb1e898e10ea7f606eff7
Calveeeeen <cc59511n@pace.edu>
2025-05-29 04:23:11 -0400

    worked on filter component. got the size and the alphabetically sorting to pass the correct query params through, need to work on displaying the filtered content

--- src/Pages/DogsPage.jsx
+++ src/Pages/DogsPage.jsx
@@ -1,66 +1,40 @@
 import { useState, useEffect } from "react";
 import DogCard from "../Components/DogCard";
 import "../Pages/DogsPage.css";
-import FilterDogs from "../Components/FilterDogs";
 import { useNavigate } from 'react-router';
 
 const DogsPage = () => {
     const navigate = useNavigate();
     const [dogs, setDogs] = useState([]);
     const [sortOrder, setSortOrder] = useState("asc");
-    // doesn't work yet
-    const [filters, setFilters] = useState({breeds: [], size: 25, from: 0 ,sort: "breed:asc"});
-
-    const handleFilterChange = (newFilters) =>{
-        setFilters(newFilters);
-    }
-
-    // auto signout after 1 hour
-    const handleLogout = async (event) => {
-        event.preventDefault();
-
-        try {
-            await fetch(
-                "https://frontend-take-home-service.fetch.com/auth/logout",
-                {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    credentials: "include",
-                }
-            ).then((res) => {
-                if (!res.ok) {
-                    throw new Error("Response Status: ", `${res.status}`);
-                } else {
-                    localStorage.removeItem("signedIn");
-                    navigate("/");
-                }
-            });
-        } catch (error) {
-            console.log(error.message);
-        }
-    }
-
-    setTimeout(() => {handleLogout()}, 3600000)
+    const [filterBreed, setFilterBreed] = useState("");
+    const [filters, setFilters] = useState({breeds: `${filterBreed}`, size: 25, form: 0, sort:`breed:${sortOrder}`});
+    const [queryParam, setQueryParam] = useState("/dogs/search?size=25&sort=breed%3Aasc")
+    const base = '/dogs/search?'
 
     useEffect(() => {
         const fetchDogs = async () => {
+            let totalRes;
+            // setQueryParam(filters);
             let dogIds = [];
-            let nextUrl = `/dogs/search?sort=breed:${sortOrder}`;
-
+            let nextUrl = queryParam;
+            // console.log(nextUrl);
             const res = await fetch(
                 `https://frontend-take-home-service.fetch.com${nextUrl}`,
                 {
                     credentials: "include",
                 }
             );
+            // console.log("nextUrl", nextUrl)
             const data = await res.json();
-            dogIds = [...dogIds, ...data.resultIds];
+            console.log("data.next url", data.next);
+            dogIds = [...data.resultIds];
             nextUrl = data.next || "";
+            // use this for the pagination, total amount of results from the database (10,000)
+            totalRes = data.total;
 
             const dogObj = await fetch(
-                "https://frontend-take-home-service.fetch.com/dogs",
+                `https://frontend-take-home-service.fetch.com/dogs`,
                 {
                     method: "POST",
                     credentials: "include",
@@ -70,18 +44,105 @@ const DogsPage = () => {
             );
             // all the dog information
             const dogData = await dogObj.json();
-            // console.log("sorted: ", dogData);
+            console.log("dogData", dogData);
             setDogs(dogData);
-            
+
+            // doesn't work 
+            // const filteredDogs = await fetch("https://frontend-take-home-service.fetch.com/dogs", 
+            //     {
+            //         method: "POST",
+            //         credentials: "include",
+            //         body: JSON.stringify(dogIds)
+            //     }
+            // );
+            // const filteredDogData = await filteredDogs.json();
+            // if(dogData.breed === filterBreed){
+            //     filteredDogData.push(dogData)
+            // }
+
+            // console.log("filtered:", filteredDogData);
         };
         fetchDogs();
-    }, [filters]);
+        console.log("new query", queryParam);
+    }, [filters, queryParam]);
 
-    
+    const handleFilterBreed = (event) => {
+        event.preventDefault();
+        console.log("enter has been hit");
+        
+        const updatedFilters = {...filters, breeds: filterBreed};
+        setFilters(updatedFilters);
+        
+
+        
+        const str = buildQuery(updatedFilters);
+        // console.log("str", str);
+        let nextUrl = base.concat(str);
+        // console.log("nextUrl",nextUrl);
+        setQueryParam(nextUrl);
+    }
+    const handleSortOrder = (event ) => {
+        const value = event.target.value;
+        console.log("value of sort: ", value);
+        const updatedFilters = {...filters, sort: `breed:${value}`}
+        setSortOrder(value);
+        setFilters(updatedFilters)
+
+        const str = buildQuery(updatedFilters);
+        let nextUrl = base.concat(str);
+        setQueryParam(nextUrl);
+    };
+
+    const handleSizeChange = (event)=>{
+        const value = parseInt(event.target.value);
+        const updatedFilters = {...filters, size: value};
+        setFilters(updatedFilters)
+        const str = buildQuery(updatedFilters);
+        let nextUrl = base.concat(str);
+        setQueryParam(nextUrl)
+    }
+
+    const buildQuery = (filters) => {
+        const param = new URLSearchParams();
+        
+        // filters.breeds.forEach((breed) => param.append("breeds", breed));
+        if(filters.breeds){
+            param.append("breed", filters.breeds)
+        }
+        if(filters.size){
+            param.append("size", filters.size)
+        }
+        if(filters.sort){
+            param.append("sort", filters.sort);
+        }
+        if(filters.from !== undefined){
+            param.append('from', filters.from);
+        }
+
+        return param.toString();
+    }
+
+    // console.log("endpoint: ", buildQuery(filters))
     return (
         <div className="body">
             <h1 className="dogPageHeader">Dogs</h1>
-            <FilterDogs onFilterChange={handleFilterChange}/>
+            <div>
+                <h1>Filter Container</h1>
+                <form onSubmit={handleFilterBreed}>
+                    <input type='text' placeholder="search the breed you're looking for" onChange={(event) => (setFilterBreed(event.target.value))} value={filterBreed}></input>
+                    <button type="submit">Submit</button>
+                </form>
+                <select onChange={handleSizeChange}>
+                    <option value={25}>25</option>
+                    <option value={50}>50</option>
+                    <option value={75}>75</option>
+                    <option value={100}>100</option>
+                </select>
+                <select onChange={handleSortOrder} value={sortOrder}>
+                    <option value="asc">Breed by A-Z</option>
+                    <option value="desc">Breed by Z-A</option>
+                </select>
+            </div>
             <div className="dogCardsContainer">
                 {dogs.length > 0 ? (
                     dogs.map((dog) => {
